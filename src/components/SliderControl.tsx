45b5bd59b05c70abc9bc656b686422b214121d25
Nikita Vologdins <nvologdins@gmail.com>
2025-01-17 19:19:15 +0200

    added html sizes for the svg arrows

--- src/components/SliderControl.tsx
+++ src/components/SliderControl.tsx
@@ -25,14 +25,14 @@ export default function SliderControl({
         onClick={decreaseHandler}
       >
         <span className="visually-hidden">Previous image</span>
-        <img src="/images/icon-angle-left.svg" alt="" />
+        <img src="/images/icon-angle-left.svg" alt="" width={14} height={24} />
       </button>
       <button
         className="controls__button controls__button_next"
         onClick={increaseHandler}
       >
         <span className="visually-hidden">Next image</span>
-        <img src="/images/icon-angle-right.svg" alt="" />
+        <img src="/images/icon-angle-right.svg" alt="" width={14} height={24} />
       </button>
     </div>
   );
